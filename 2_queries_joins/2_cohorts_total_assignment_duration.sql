d4f18b62f3cb2db2231ee37648222834208a9dfd
nasouh94 <nasouh@live.com>
2019-10-14 19:29:11 +0000

    in the total assignemnt querey added a querey that uses having to count

--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -3,3 +3,10 @@ FROM assignment_submissions
 JOIN students on students.id = student_id
 JOIN cohorts on cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
